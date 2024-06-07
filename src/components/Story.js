cc07d775f1ee3bd709a5346c8607cdea80313629
Soraia <sorilc@hotmail.com>
2024-06-07 16:13:38 +0200

    fix

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,7 @@ import cookers2 from '../assets/cookers2.jpg'
 
 function Story() {
     return(
-        <section className="px-6 flex-col lg:mb-10 2xl:px-56 2xl:mt-80 mt-36 lg:mt-56 sm:mt-40 flex lg:flex-row lg:h-[30em] 2xl:h-[35em] sm:flex-col sm:justify-center sm:items-center" id='about'>
+        <section className="px-6 pb-16 flex-col lg:mb-10 2xl:px-56 2xl:mt-80 mt-36 lg:mt-56 sm:mt-40 flex lg:flex-row lg:h-[30em] 2xl:h-[38em] sm:flex-col sm:justify-center sm:items-center" id='about'>
             <div className="flex flex-col 2xl:h-full lg:h-full lg:w-2/5 2xl:w-1/2 sm:w-3/5">
                 <h1 className="text-primary-green text-3xl sm:text-4xl font-bold text-center sm:text-left">Our Story</h1>
                 <p className="mt-8 text-center sm:text-left 2xl:text-2xl font-medium 2xl:w-9/12 lg:text-lg">
