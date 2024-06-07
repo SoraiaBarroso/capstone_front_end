eb20e36e37b5c06171ac0c8c0b66c8802ae6f50b
Soraia <sorilc@hotmail.com>
2024-06-07 16:10:08 +0200

    fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ function Footer() {
     ];
 
     return(
-        <div className="h-32 md:mt-48 border-t-2 2xl:mt-36 2xl:px-56">
+        <div className="h-32  border-t-2 2xl:px-56">
             <div className='flex justify-between items-center flex-col md:flex-row sm:flex-col sm:items-center w-full mt-10 mb-10 gap-20'>
                 <Link to="/">
                     <img loading="lazy" src={logoFooter} alt='logo footer' className='h-44 md:h-28 md:ml-10 lg:h-36 lg:pl-32 2xl:pl-20 2xl:pr-40'></img>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ function Main(){
     return (
             <section className='px-6 flex lg:px-20 2xl:px-56 2xl:h-[25em] sm:px-8 h-96 text-white bg-primary-green' id='home'>
                 <div className="h-full flex flex-col items-center sm:items-start w-full m-0 md:w-1/2 sm:w-1/2 lg:w-1/2 2xl:w-1/2">
-                    <h1 className='text-primary-yellow text-3xl text-center sm:text-left font-serif md:font-bold mt-10 2xl:text-5xl md:text-4xl sm:text-4xl'>Little Lemon</h1>
+                    <h1 className='text-primary-yellow text-4xl font-bold text-center sm:text-left font-serif md:font-bold mt-10 2xl:text-5xl md:text-4xl sm:text-4xl'>Little Lemon</h1>
                     <h2 className='mt-2 text-center sm:text-left 2xl:text-2xl text-lg sm:text-lg font-medium'>Chicago</h2>
                     <p className='2xl:w-3/4 text-md text-center sm:text-left lg:text-lg md:text-base 2xl:text-lg sm:w-full 2xl:text-md sm:text-sm mt-6'>
                         Welcome to our family-owned Mediterranean restaurant, where tradition meets innovation.
