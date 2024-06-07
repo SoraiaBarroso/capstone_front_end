611d4396625ec54ef8fe6317d7787f448903d39d
Soraia <sorilc@hotmail.com>
2024-06-07 11:53:55 +0200

    navbar fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ function NavBar() {
             <div className='flex justify-center items-center lg:hidden 2xl:hidden cursor-pointer group' onClick={handleMenuToggle}>
               <RxHamburgerMenu size="2em"/>
             </div>
-            <ul className={`fixed w-1/3 z-40 top-0 left-0 h-full bg-white flex flex-col transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} [&>*]:mt-10 text-lg lg:static lg:flex-row lg:transform-none lg:translate-x-0 lg:bg-transparent lg:w-3/5 lg:h-full lg:[&>*]:m-0 lg:flex lg:justify-between lg:items-center items-center sm:font-semibold sm:text-text-black sm:text-lg md:w-[18em] justify-start sm:justify-start sm:w-[18em] 2xl:w-3/5`}>
+            <ul className={`fixed w-1/2 z-40 top-0 left-0 h-full bg-white flex flex-col transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} [&>*]:mt-10 text-lg lg:static lg:flex-row lg:transform-none lg:translate-x-0 lg:bg-transparent lg:w-3/5 lg:h-full lg:[&>*]:m-0 lg:flex lg:justify-between lg:items-center items-center sm:font-semibold sm:text-text-black sm:text-lg md:w-[18em] justify-start sm:justify-start sm:w-[18em] 2xl:w-3/5`}>
                 <li><Link  to="/"  onClick={handleMenuToggle} className='hover:text-primary-green'>Home</Link></li>
                 <li><a href='/#about' onClick={handleMenuToggle} className='hover:text-primary-green'>About</a></li>
                 <li><a href='/#menu' onClick={handleMenuToggle} className='hover:text-primary-green'>Menu</a></li>
