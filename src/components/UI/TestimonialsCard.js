ed23eba22c612f2ff941f64b3a08b0f3a7be2e60
Soraia <sorilc@hotmail.com>
2024-06-08 13:56:22 +0200

    fix

--- src/components/UI/TestimonialsCard.js
+++ src/components/UI/TestimonialsCard.js
@@ -6,7 +6,7 @@ function TestimonalsCard({name, review, srcPicture}) {
     return(
         <div className="bg-primary-green place-self-center w-72 h-52 2xl:w-[24em] lg:w-[18em] lg:h-[14em] col-span-1 sm:w-[24em] sm:place-self-center sm:mt-10 rounded-xl row-span-2 flex flex-col justify-center items-center" data-aos="zoom-in">
             <div className='flex flex-col w-full justify-center items-center transform -translate-y-10'>
-                <img loading="lazy" src={srcPicture} alt="user testimonial" className='rounded-full w-24 h-24 object-cover !overflow-x-visible'></img>
+                <img loading="lazy" src={srcPicture} alt="user testimonial" className='rounded-full w-24 h-24 object-cover'></img>
                 <h2 className='text-white text-lg mt-2 sm:mt-4'>{name}</h2>
                 <IconContext.Provider value={{ color: "yellow"}}>
                     <div className='flex mt-1'>
